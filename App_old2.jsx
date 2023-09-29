92343256b772ba8b85117eb51fa9ac8662d9c776
Anne-Natchimie <natchimie.annelr@tutanota.com>
2023-09-29 09:41:12 -0400

    First

---
+++ App_old2.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Tuto from './tuto/Tuto'
+
+const App = () => {
+    return (
+        <Tuto /> 
+    )
+}
+
+export default App
