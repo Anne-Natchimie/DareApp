5d3839b8ac338e5697b8ebce75e23d524cf179a5
Anne-Natchimie <natchimie.annelr@tutanota.com>
2023-10-02 10:45:45 -0400

    Four

---
+++ tuto/product/component/CardProduct.jsx
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native'
+import { Avatar, Button, Card } from 'react-native-paper'
+import { stylesProduct } from '../stylesProduct'
+
+import React from 'react'
+
+const CardProduct = ({product}) => {
+    return (
+        <Card style={stylesProduct.cardProduct} contentStyle={stylesProduct.cardContent} >
+            {/* <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} /> */}
+                    <Card.Cover source={{ uri: product.thumbnail }} />
+                    <Card.Content>
+                        <Text variant="titleLarge" style={stylesProduct.cardTitle}>{product.title}</Text>
+                        <Text variant="bodyMedium" style={stylesProduct.cardPrice}>{product.price}</Text>
+                    </Card.Content>
+            {/* <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+            </Card.Actions> */}
+        </Card>
+    )
+}
+
+export default CardProduct
